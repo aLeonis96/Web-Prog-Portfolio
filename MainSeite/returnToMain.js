f1f63aa32eb98dbf5500e98273cad1cd2a2f020a
aLeonis96 <136602422+aLeonis96@users.noreply.github.com>
2023-10-28 11:40:18 +0200

    added functionality to return to Main, and finished serched Result without throwing error

---
+++ MainSeite/returnToMain.js
@@ -0,0 +1,14 @@
+
+    
+    const logo = document.querySelector('.brandname img ');
+    const CompanyName = document.querySelector('.brandname h1 ');
+    
+    logo.addEventListener('click', () => {
+        
+        window.location.href = 'MainSeite.html'; 
+    });
+
+    CompanyName.addEventListener('click', () => {
+        
+        window.location.href = 'MainSeite.html'; 
+    });
--- MainSeite/scriptDisplaySearchResult.js
+++ MainSeite/scriptDisplaySearchResult.js
@@ -2,8 +2,52 @@
 const urlParams = new URLSearchParams(window.location.search);
 const searchText = urlParams.get('search');
 
-console.log(searchText);
+executeSearch();
 
-fetch('https://dummyjson.com/products/category/' + searchText + '?/limit=0')
-            .then(res => res.json())
-            .then(console.log);
+
+
+function executeSearch(){
+    fetch('https://dummyjson.com/products/category/' + searchText ) 
+             .then(res => res.json())
+                .then(data => {
+                    const response = data.products;
+                    
+                    category = document.getElementById('category');
+                    category.innerHTML= response[0].category;
+                    
+                    response.forEach(element => {
+                        
+                        const imgDiv = document.createElement('div')
+                        imgDiv.className = 'produktImageSmall';
+
+                        const title = document.createElement('div');
+                        title.className = 'produktTitle';
+                        title.innerHTML = element.title;
+                        
+                        const img = document.createElement('img');
+                        img.src= element.images[1];
+                        imgDiv.appendChild(img);
+
+                        const produkt = document.createElement('div');
+                        produkt.className='produkt';
+                        produkt.id = element.id;
+
+                        produktActions = document.createElement('div');
+                        produktActions.className='produktActions' ;
+                        produktActions.innerHTML = '<a href="" class="hearticon-"></a> <a href="" class="carticon-"></a>'
+
+                        produkt.appendChild(imgDiv);
+                        produkt.appendChild(title);
+                        produkt.appendChild(produktActions);
+                        
+                        productsContainer.appendChild(produkt);
+
+                        produkt.addEventListener('click', () => {
+                            // Redirect to the product details page
+                            window.location.href = 'DisplayProduktDetails.html?id=' + element.id;
+                            
+                          });
+                    })
+                });
+}
+
